20732c9800e7533f8fbf8597e64144956aad3684
kimayaa29 <magic.29@rediffmail.com>
2020-09-03 20:18:39 +0530

    added subnet variables

--- variables.tf
+++ variables.tf
@@ -2,6 +2,10 @@ variable "region" {
     default = "ap-south-1"
 }
 
-variable "cidr-block"{
-    default = "10.0.0.0/16"
-}
+variable "cidr-block"{}
+variable "public-subnet1-cidr" {}
+variable "public-subnet2-cidr" {}
+variable "public-subnet3-cidr" {}
+variable "private-subnet1-cidr" {}
+variable "private-subnet2-cidr" {}
+variable "private-subnet3-cidr" {}
